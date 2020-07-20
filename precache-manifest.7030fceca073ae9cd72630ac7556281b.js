9ab38a7704511550086de4e589992cacea880c67
AlexDeni <alexdenisenko94@gmail.com>
2020-07-21 01:04:24 +0300

    Updates

---
+++ precache-manifest.7030fceca073ae9cd72630ac7556281b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01c3e2948931529087c0cddefcaba4e7",
+    "url": "/testAvia/index.html"
+  },
+  {
+    "revision": "7df5399f0e5efc98099a",
+    "url": "/testAvia/static/css/main.d736fa31.chunk.css"
+  },
+  {
+    "revision": "cef5f1e103fab8f68861",
+    "url": "/testAvia/static/js/2.e0ed7a90.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/testAvia/static/js/2.e0ed7a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7df5399f0e5efc98099a",
+    "url": "/testAvia/static/js/main.bdc811d6.chunk.js"
+  },
+  {
+    "revision": "b95928609ec2dd141a5f",
+    "url": "/testAvia/static/js/runtime-main.afa3647e.js"
+  },
+  {
+    "revision": "843f481c824f86f2b05d2d34e3890e6c",
+    "url": "/testAvia/static/media/defaultPlane.843f481c.png"
+  },
+  {
+    "revision": "5906fb82e31864c9b531898bb3c97d98",
+    "url": "/testAvia/static/media/monzerat.5906fb82.ttf"
+  }
+]);
